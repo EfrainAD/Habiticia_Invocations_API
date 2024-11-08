5ab9f13d1b6c790c56b257101ee22bbc7e82e409
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2024-11-08 14:45:22 -0500

    Set up server

---
+++ server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import express from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.get('/', (req, res) => {
+   res.send('Hello, world!')
+})
+
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`)
+})
