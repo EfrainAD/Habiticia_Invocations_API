80cd6f07979ee2739f09d4f38791ad81b93482e1
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2024-11-11 01:04:44 -0500

    Added cron path that check to make sure you finished all dailies. And added some comments in the code.

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+// .env Setup
 import dotEnv from 'dotenv'
 import { expand } from 'dotenv-expand'
 expand(dotEnv.config())
 
+// Imports
 import express from 'express'
 import axios from 'axios'
 import asyncHandler from 'express-async-handler'
 
+// Variables
 const app = express()
 const PORT = process.env.PORT || 8000
 const headers = {
@@ -14,10 +17,12 @@ const headers = {
    'x-client': process.env.HABITICA_CLIENT,
 }
 
+// Check that the server is running
 app.get('/', (req, res) => {
-   res.send(`Hello, world! ${process.env.HABITICA_CLIENT}`)
+   res.send(`Hello, world! This is ${process.env.HABITICA_CLIENT}`)
 })
 
+// Check connectiong to habitica
 app.get(
    '/habitica',
    asyncHandler(async (req, res) => {
@@ -29,6 +34,38 @@ app.get(
    })
 )
 
+app.get(
+   '/run-cron-task',
+   asyncHandler(async (req, res) => {
+      // Get Dailies from Habitica
+      const response = await axios.get(
+         'https://habitica.com/api/v3/tasks/user?type=dailys&isDue=true',
+         { headers }
+      )
+
+      // Filter to only have Dailies that still need to be done today
+      const dueDailies = response.data.data.filter(
+         (task) => task.isDue && !task.completed
+      )
+      const dueDailiesCount = dueDailies.length
+
+      // Run cron
+      if (dueDailiesCount === 0) {
+         const cronResponse = await axios.post(
+            'https://habitica.com/api/v3/cron',
+            {},
+            { headers }
+         )
+         res.json(cronResponse.data)
+      } else {
+         res.json({
+            message: ` Cancled do to haveing ${dueDailiesCount} dailies not competed.`,
+         })
+      }
+   })
+)
+
+// Error Handling Middleware
 app.use((error, req, res, next) => {
    res.status(500).json({
       message: error.message,
@@ -36,6 +73,7 @@ app.use((error, req, res, next) => {
    })
 })
 
+// Start the server
 app.listen(PORT, () => {
    console.log(
       `Server is running on port ${PORT} ${
