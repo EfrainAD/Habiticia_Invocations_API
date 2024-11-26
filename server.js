17f540a53c6ef49290fca93887e5fbd3012026fd
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2024-11-26 04:26:53 -0500

    Now know what gear to use that will have the highest INT value for a rague

--- server.js
+++ server.js
@@ -34,6 +34,149 @@ app.get(
    })
 )
 
+// Test "test" NO CALL
+function run(obj, results = {}) {
+   if (typeof obj === 'object' && !obj.hasOwnProperty('int')) {
+      for (const key in obj) {
+         results = run(obj[key], results)
+      }
+   } else {
+      console.log('yo', obj)
+      console.log(results.int, obj.int)
+      if (results.int < obj.int || results.int === undefined) {
+         console.log(results.int, '<', obj.int)
+         results = obj
+      }
+   }
+   return results
+}
+const testObj = {
+   a: { int: 2, key: 'c oool' },
+   b: {
+      b1: { b12: { key: 'by12', int: 30 }, b11: { key: 'by11', int: 2 } },
+      b2: { key: 'by2', int: 23 },
+   },
+   c: { key: 'bat', int: 0 },
+}
+// console.log(run(testObj))
+const runAxios = asyncHandler(async () => {
+   const respons = await axios.get(
+      'https://habitica.com/api/v3/user/anonymized',
+      {
+         headers,
+      }
+   )
+   const ownedItems = Object.keys(respons.data.data.user.items.gear.owned)
+   console.log({ ownedItems })
+})
+// runAxios()
+// Test "test"
+app.get(
+   '/test',
+   asyncHandler(async (req, res) => {
+      const respons = await axios.get(
+         'https://habitica.com/api/v3/user/anonymized',
+         { headers }
+      )
+
+      const postCronGear = respons.data.data.user.items.gear.equipped
+      const ownedGear = Object.keys(respons.data.data.user.items.gear.owned)
+
+      const habiticaContent = await axios.get(
+         'https://habitica.com/api/v3/content'
+      )
+      const habiticaGearInfo = habiticaContent.data.data.gear.tree
+      const ownedHighestIntGear = getUserHighestIntEquipment(habiticaGearInfo)
+
+      // Pick one hand weapon or two handed weapon
+      // Get the INT values to evaluate best weapon choice
+      const oneHandedInt = ownedHighestIntGear.weapon.oneHanded?.int || 0
+      const twoHandedInt = ownedHighestIntGear.weapon.twoHanded?.int || 0
+      const shieldInt = ownedHighestIntGear.shield?.int || 0
+
+      // Calculate the weapon setup to use at cron
+      const cronGear =
+         oneHandedInt + shieldInt > twoHandedInt
+            ? {
+                 ...ownedHighestIntGear,
+                 weapon: ownedHighestIntGear.weapon.oneHanded,
+                 shield: ownedHighestIntGear.shield,
+              }
+            : {
+                 ...ownedHighestIntGear,
+                 weapon: ownedHighestIntGear.weapon.twoHanded,
+                 shield: {},
+              }
+
+      function getUserHighestIntEquipment(
+         obj,
+         results = {
+            weapon: { oneHanded: {}, twoHanded: {} },
+            armor: {},
+            head: {},
+            shield: {},
+            back: {},
+            body: {},
+            headAccessory: {},
+            eyewear: {},
+         }
+      ) {
+         // Drill to last object
+         if (typeof obj === 'object' && !obj.hasOwnProperty('int')) {
+            for (const key in obj) {
+               results = getUserHighestIntEquipment(obj[key], results)
+            }
+         } else {
+            // Now have the last nested object
+            // Check if owner owns it, and if gear is an int buff gear.
+            if (ownedGear.includes(obj.key) && obj.int > 0) {
+               const { type, twoHanded } = obj
+               if (type === 'weapon') {
+                  if (twoHanded) {
+                     if (
+                        results.weapon.twoHanded?.int < obj.int ||
+                        Object.keys(results.weapon.twoHanded).length === 0
+                     ) {
+                        results = {
+                           ...results,
+                           weapon: { ...results.weapon, twoHanded: obj },
+                        }
+                     }
+                  } else {
+                     if (
+                        results.weapon.oneHanded?.int < obj.int ||
+                        Object.keys(results.weapon.oneHanded).length === 0
+                     ) {
+                        results = {
+                           ...results,
+                           [type]: { ...results.weapon, oneHanded: obj },
+                        }
+                     }
+                  }
+               } else {
+                  // Every other gear that not type weapon
+                  if (
+                     results[type].int < obj.int ||
+                     Object.keys(results[type]).length === 0
+                  ) {
+                     results = { ...results, [type]: obj }
+                  }
+               }
+            }
+         }
+         return results
+      }
+
+      res.json({
+         cronGear: cronGear,
+         postCronGear: postCronGear,
+         todaysDropCount: respons.data.data.user.items.lastDrop.count,
+         siteData: habiticaGearInfo,
+      })
+   })
+)
+
+// Custom cron "run-cron-task"
 app.get(
    '/run-cron-task',
    asyncHandler(async (req, res) => {
@@ -68,8 +211,9 @@ app.get(
 // Error Handling Middleware
 app.use((error, req, res, next) => {
    res.status(500).json({
-      message: error.message,
-      errorObj: process.env.NODE_ENV === 'development' ? error : null,
+      message: error.message || 'An unexpected error occurred',
+      details: error.response?.data || null,
+      stack: process.env.NODE_ENV === 'development' ? error.stack : null,
    })
 })
 
