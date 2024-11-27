9b20c371e071562d81eb645cf0a91c9365463f5e
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2024-11-27 15:54:28 -0500

    Add route that will set the users equipped gear to be the setup to give them the highest INT from the gear they have

--- server.js
+++ server.js
@@ -74,41 +74,7 @@ const runAxios = asyncHandler(async () => {
 app.get(
    '/test',
    asyncHandler(async (req, res) => {
-      const respons = await axios.get(
-         'https://habitica.com/api/v3/user/anonymized',
-         { headers }
-      )
-
-      const postCronGear = respons.data.data.user.items.gear.equipped
-      const ownedGear = Object.keys(respons.data.data.user.items.gear.owned)
-
-      const habiticaContent = await axios.get(
-         'https://habitica.com/api/v3/content'
-      )
-      const habiticaGearInfo = habiticaContent.data.data.gear.tree
-      const ownedHighestIntGear = getUserHighestIntEquipment(habiticaGearInfo)
-
-      // Pick one hand weapon or two handed weapon
-      // Get the INT values to evaluate best weapon choice
-      const oneHandedInt = ownedHighestIntGear.weapon.oneHanded?.int || 0
-      const twoHandedInt = ownedHighestIntGear.weapon.twoHanded?.int || 0
-      const shieldInt = ownedHighestIntGear.shield?.int || 0
-
-      // Calculate the weapon setup to use at cron
-      const cronGear =
-         oneHandedInt + shieldInt > twoHandedInt
-            ? {
-                 ...ownedHighestIntGear,
-                 weapon: ownedHighestIntGear.weapon.oneHanded,
-                 shield: ownedHighestIntGear.shield,
-              }
-            : {
-                 ...ownedHighestIntGear,
-                 weapon: ownedHighestIntGear.weapon.twoHanded,
-                 shield: {},
-              }
-
-      function getUserHighestIntEquipment(
+      const getUserHighestIntEquipment = (
          obj,
          results = {
             weapon: { oneHanded: {}, twoHanded: {} },
@@ -120,7 +86,7 @@ app.get(
             headAccessory: {},
             eyewear: {},
          }
-      ) {
+      ) => {
          // Drill to last object
          if (typeof obj === 'object' && !obj.hasOwnProperty('int')) {
             for (const key in obj) {
@@ -167,11 +133,163 @@ app.get(
          return results
       }
 
+      const respons = await axios.get(
+         'https://habitica.com/api/v3/user/anonymized',
+         { headers }
+      )
+
+      const postCronGear = respons.data.data.user.items.gear.equipped
+      // const ownedGear = Object.keys(respons.data.data.user.items.gear.owned)
+
+      // const habiticaContent = await axios.get(
+      //    'https://habitica.com/api/v3/content'
+      // )
+      // const habiticaGearInfo = habiticaContent.data.data.gear.tree
+      // const ownedHighestIntGear = getUserHighestIntEquipment(habiticaGearInfo)
+
+      // Pick one hand weapon or two handed weapon
+      // Get the INT values to evaluate best weapon choice
+      // const oneHandedInt = ownedHighestIntGear.weapon.oneHanded?.int || 0
+      // const twoHandedInt = ownedHighestIntGear.weapon.twoHanded?.int || 0
+      // const shieldInt = ownedHighestIntGear.shield?.int || 0
+
+      // Calculate the weapon setup to use at cron
+      // const cronGear =
+      //    oneHandedInt + shieldInt > twoHandedInt
+      //       ? {
+      //            ...ownedHighestIntGear,
+      //            weapon: ownedHighestIntGear.weapon.oneHanded,
+      //            shield: ownedHighestIntGear.shield,
+      //         }
+      //       : {
+      //            ...ownedHighestIntGear,
+      //            weapon: ownedHighestIntGear.weapon.twoHanded,
+      //            shield: {},
+      //         }
+
       res.json({
-         cronGear: cronGear,
+         // cronGear: cronGear,
          postCronGear: postCronGear,
-         todaysDropCount: respons.data.data.user.items.lastDrop.count,
-         siteData: habiticaGearInfo,
+         // todaysDropCount: respons.data.data.user.items.lastDrop.count,
+         // siteData: habiticaGearInfo,
+      })
+   })
+)
+
+// change weapon to the highest Int
+app.post(
+   '/changeWeaponInt',
+   asyncHandler(async (req, res) => {
+      const getUserHighestIntEquipment = (
+         obj,
+         results = {
+            weapon: { oneHanded: {}, twoHanded: {} },
+            armor: {},
+            head: {},
+            shield: {},
+            back: {},
+            body: {},
+            headAccessory: {},
+            eyewear: {},
+         }
+      ) => {
+         // Drill to last object
+         if (typeof obj === 'object' && !obj.hasOwnProperty('int')) {
+            for (const key in obj) {
+               results = getUserHighestIntEquipment(obj[key], results)
+            }
+         } else {
+            // Now have the last nested object
+            // Check if owner owns it, and if gear is an int buff gear.
+            if (ownedGear.includes(obj.key) && obj.int > 0) {
+               const { type, twoHanded } = obj
+               if (type === 'weapon') {
+                  if (twoHanded) {
+                     if (
+                        results.weapon.twoHanded?.int < obj.int ||
+                        Object.keys(results.weapon.twoHanded).length === 0
+                     ) {
+                        results = {
+                           ...results,
+                           weapon: { ...results.weapon, twoHanded: obj },
+                        }
+                     }
+                  } else {
+                     if (
+                        results.weapon.oneHanded?.int < obj.int ||
+                        Object.keys(results.weapon.oneHanded).length === 0
+                     ) {
+                        results = {
+                           ...results,
+                           [type]: { ...results.weapon, oneHanded: obj },
+                        }
+                     }
+                  }
+               } else {
+                  // Every other gear that not type weapon
+                  if (
+                     results[type].int < obj.int ||
+                     Object.keys(results[type]).length === 0
+                  ) {
+                     results = { ...results, [type]: obj }
+                  }
+               }
+            }
+         }
+         return results
+      }
+
+      const userData = await axios.get(
+         'https://habitica.com/api/v3/user/anonymized',
+         { headers }
+      )
+      const currentGear = userData.data.data.user.items.gear.equipped
+      const ownedGear = Object.keys(userData.data.data.user.items.gear.owned)
+
+      const habiticaContent = await axios.get(
+         'https://habitica.com/api/v3/content'
+      )
+      const habiticaGearInfo = habiticaContent.data.data.gear.tree
+
+      const ownedHighestIntGear = getUserHighestIntEquipment(habiticaGearInfo)
+
+      // Pick one hand weapon or two handed weapon
+      // Get the INT values to evaluate best weapon choice
+      const oneHandedInt = ownedHighestIntGear.weapon.oneHanded?.int || 0
+      const twoHandedInt = ownedHighestIntGear.weapon.twoHanded?.int || 0
+      const shieldInt = ownedHighestIntGear.shield?.int || 0
+
+      // Calculate the weapon setup to use at cron
+      const highestIntGear =
+         oneHandedInt + shieldInt > twoHandedInt
+            ? {
+                 ...ownedHighestIntGear,
+                 weapon: ownedHighestIntGear.weapon.oneHanded,
+                 shield: ownedHighestIntGear.shield,
+              }
+            : {
+                 ...ownedHighestIntGear,
+                 weapon: ownedHighestIntGear.weapon.twoHanded,
+                 shield: {},
+              }
+
+      const keys = Object.keys(highestIntGear)
+      for (let i = 0; i < keys.length; i++) {
+         const key = keys[i]
+         if (
+            highestIntGear[key].key &&
+            highestIntGear[key].key !== currentGear[key]
+         ) {
+            const res = await axios.post(
+               `https://habitica.com/api/v3/user/equip/equipped/${highestIntGear[key].key}`,
+               {},
+               { headers }
+            )
+         }
+      }
+
+      res.json({
+         message: 'Request was sent',
       })
    })
 )
